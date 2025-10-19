a2fed67255a8c96852d249e708c401fa9f44ce4b
Priyank1905 <priyankjethva1905@gmail.com>
2025-10-19 12:36:18 +0530

    Implement new components and animations for enhanced user experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
+import { BackgroundRippleEffect } from "@/components/ui/background-ripple-effect";
+
+import TestimonialCards from "@/components/TestimonialCards";
 
 import Image from "next/image";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection />
       <FeaturedCourses />
+      <WhyChooseUs />
+      
+        <TestimonialCards />
+      
     </main>
   );
 }
